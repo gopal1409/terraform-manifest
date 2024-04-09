8559deff52d92382a4ffe1a109f5af32f119248f
gopal <gopal1409@gmail.com>
2024-04-09 14:48:54 +0530

    terraform ec2 instance

--- main.tf
+++ main.tf
@@ -17,11 +17,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 # Create a VPC
+
+##this is the resource block
 resource "aws_instance" "myec2vm" {
-  ami = "ami-080e1f13689e07408"
-  instance_type = "t2.micro"
+  ##once your instance get created i want to store the instance state information
+  ##we need to lable the information
+  ###inside the resource block we need to define argument in 
+  ##key value format
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t3.micro"
   tags = {
-    "Name" =  "Ec2 demo"
+    "Name" = "Ec2 demo-1"
   }
 }
