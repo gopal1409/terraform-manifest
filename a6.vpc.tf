7e1fcc7db80f18a64efe3e5dbf6d3f04d7b6feda
gopal <gopal1409@gmail.com>
2024-04-11 15:48:40 +0530

    terrafomr with subnet count length index

--- a6.vpc.tf
+++ a6.vpc.tf
@@ -1,3 +1,4 @@
+##this is vpc cidr
 resource "aws_vpc" "this" {
   cidr_block = var.cidr
   enable_dns_support = true 
@@ -7,6 +8,7 @@ resource "aws_vpc" "this" {
   }
 }
 
+#this si subnet will create multiple subnet
 resource "aws_subnet" "private" {
   count = length(var.subnet_cidr_private) #3 ##count required what a numric value
   # ["10.20.20.0/28", "10.20.20.16/28", "10.20.20.32/28"]
@@ -19,3 +21,23 @@ resource "aws_subnet" "private" {
     Name = "Subnet-${count.index}"
   }
 }
+
+##we will create an custom route table
+resource "aws_route_table" "this-rt" {
+  vpc_id = aws_vpc.this.id 
+  tags = {
+    Name = "my-app-custom-rtb"
+  }
+}
+##once i create the custom rtb i need to attach my subnet with the custom rtb private subnet
+resource "aws_route_table_association" "this-rt-assoc" {
+   ##i need to attach all the subnet we will use the count
+   count = length(var.subnet_cidr_private)
+   ##now i need to attach all my subnet one by one with the route table
+   ##we already have all the subnet available
+   #element is used to call the existing resource where as for_each will be the creator
+   subnet_id = element(aws_subnet.private.*.id,count.index)
+   #finally attach 
+   route_table_id = aws_route_table.this-rt.id 
+}
+
