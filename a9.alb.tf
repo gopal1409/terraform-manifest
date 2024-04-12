039edb1c446ffed799a0c97be8e364126191a4ef
gopal <gopal1409@gmail.com>
2024-04-12 15:47:42 +0530

    alb

--- a9.alb.tf
+++ a9.alb.tf
@@ -24,3 +24,24 @@ resource "aws_alb_target_group_attachment" "attach-app-servers" {
   target_id = element(aws_instance.ec2demo.*.id,count.index)
   port = 80 
 }
+
+###first we will create a listener whcih is like a security groupo for your lb
+resource "aws_lb_listener" "list-alb" {
+  load_balancer_arn = aws_lb.lb-front.arn
+  port = "80"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.front-lb.arn 
+  }
+}
+
+#create the load balancer 
+resource "aws_lb" "lb-front" {
+  name = "myalb"
+  internal = false 
+  load_balancer_type = "application"
+  security_groups =  [aws_security_group.webserver.id]
+  subnets = [for subnet in aws_subnet.private :  subnet.id]
+  enable_deletion_protection = false 
+}
