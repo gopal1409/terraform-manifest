1706f5cf440c546536ccf208c51772fb5fdca892
gopal <gopal1409@gmail.com>
2024-04-17 16:29:47 +0530

    default vpc

--- vpc/a4.subnet.tf
+++ vpc/a4.subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_subnet" {
   count             = 2 #whole number 2
-  vpc_id            = aws_vpc.vpc.id
+  vpc_id            = aws_vpc.aws_default_vpc.id
   cidr_block        = var.public_subnet_cidr[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   count             = 2 #whole number 2
-  vpc_id            = aws_vpc.vpc.id
+  vpc_id            = aws_vpc.aws_default_vpc.id
   cidr_block        = var.private_subnet_cidr[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
