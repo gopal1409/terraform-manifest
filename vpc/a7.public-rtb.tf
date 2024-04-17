ca166ef887e88849eeda94ae0c50590f1c2008ae
gopal <gopal1409@gmail.com>
2024-04-17 16:31:37 +0530

    default vpc

--- vpc/a7.public-rtb.tf
+++ vpc/a7.public-rtb.tf
@@ -1,12 +1,12 @@
 resource "aws_internet_gateway" "inter_gateway" {
-  vpc_id = aws_vpc.aws_default_vpc.id
+  vpc_id = aws_default_vpc.vpc.id
   tags = {
     Name = "${var.environment}-internet-gw"
   }
 }
 
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.aws_default_vpc.id
+  vpc_id = aws_default_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.inter_gateway.id
